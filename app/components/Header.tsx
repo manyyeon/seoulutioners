c38dd415cac2254774ff6a04a8f7f67e1b23752a
manyyeon <udayeon@naver.com>
2024-07-24 12:21:12 +0700

    💄 Update about page ui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,13 @@ const Header: React.FC = () => {
   return (
     <header className='bg-black text-white p-4 flex justify-between'>
       <Image src={'/assets/images/logo.png'} alt='seoulutioners' width={200} height={800} />
-      <nav className='flex gap-2 m-2'>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
+      <nav className='flex gap-2 m-2 pr-4 text-lg'>
+        <Link href='/' className='hover:text-xl'>
+          ABOUT US
+        </Link>
+        <Link href='/products' className='hover:text-xl'>
+          PRODUCTS
+        </Link>
       </nav>
     </header>
   );
