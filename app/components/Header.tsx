2e55fced079782f5703ed5750e50d8d40cd5ab5a
manyyeon <udayeon@naver.com>
2024-07-24 21:50:20 +0700

    💄 Update nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,20 +1,40 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const pathname = usePathname();
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <header className='bg-black text-white p-4 flex flex-col md:flex-row justify-between fixed w-full'>
-      <div className='flex justify-center md:justify-start'>
+      <div className='flex justify-between items-center w-full md:w-auto'>
         <Image src={'/assets/images/logo.png'} alt='SOLA' width={200} height={50} className='h-auto' />
+        <button className='md:hidden text-white' onClick={toggleMenu}>
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7' />
+          </svg>
+        </button>
       </div>
-      <nav className='flex flex-col md:flex-row gap-2 md:gap-4 m-2 pr-4 text-lg'>
-        <Link href='/' className={`hover:text-xl ${pathname === '/' ? 'text-gold' : 'text-white'}`}>
+      <nav
+        className={`flex flex-col md:flex-row gap-2 md:gap-4 m-2 pr-4 text-lg ${
+          menuOpen ? 'block' : 'hidden'
+        } md:block `}
+      >
+        <Link href='/' className={`hover:text-xl md:mr-4 ${pathname === '/' ? 'text-gold' : 'text-white'}`}>
           ABOUT US
         </Link>
         <Link href='/products' className={`hover:text-xl ${pathname === '/products' ? 'text-gold' : 'text-white'}`}>
