cc416915fbef2b1b69d4c45c721fdbe3e85367be
Dayeon Yoo <udayeon@naver.com>
2024-07-24 09:57:05 +0700

    Feature/base design materials (#1)
    * 🍱 Add fonts
    
    * 🍱 Add logo
    
    * 🔧 Set pretendard font
    
    * ✨ Add header
    
    * 🔧 Update next.config
    
    * 🍱 Add logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-black text-white p-4 flex justify-between">
-      <h1 className="text-xl">My Company</h1>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+    <header className='bg-black text-white p-4 flex justify-between'>
+      <Image src={'/assets/images/logo.png'} alt='seoulutioners' width={200} height={800} />
+      <nav className='flex gap-2 m-2'>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
       </nav>
     </header>
   );
