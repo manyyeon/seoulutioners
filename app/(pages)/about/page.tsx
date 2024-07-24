317aad1bcc35eeb8b307f05b4830e97072caaed2
manyyeon <udayeon@naver.com>
2024-07-24 09:33:32 +0700

    ✨ Add header

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/app/components/Header";
-import Head from "next/head";
+import Header from '@/app/components/Header';
+import Head from 'next/head';
 
 const About: React.FC = () => {
   return (
@@ -8,8 +8,8 @@ const About: React.FC = () => {
         <title>About Us - My Company</title>
       </Head>
       <Header />
-      <main className="p-4">
-        <h2 className="text-2xl">About Us</h2>
+      <main className='p-4'>
+        <h2 className='text-2xl'>About Us</h2>
         <p>Information about our company.</p>
       </main>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Head from "next/head";
-import Header from "./components/Header";
+import Head from 'next/head';
+import Header from './components/Header';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>My Company</title>
-        <meta name="description" content="Welcome to My Company" />
+        <title>Seoulutioners</title>
+        <meta name='description' content='Welcome to My Company' />
       </Head>
       <Header />
-      <main className="p-4">
-        <h2 className="text-2xl">Welcome to My Company</h2>
+      <main className='p-4'>
+        <h2 className='text-2xl'>Welcome to My Company</h2>
         <p>We provide the best services in the industry.</p>
       </main>
     </div>
