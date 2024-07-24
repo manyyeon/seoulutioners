fd65e98090ecf07a01a5136e4f50b2e03ccd798a
manyyeon <udayeon@naver.com>
2024-07-24 21:34:48 +0700

    💄 Add responsive size to set the mobile size

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -21,57 +21,75 @@ const Products: React.FC = () => {
           />
         </div>
 
-        <div className='px-8'>
-          <div className='flex flex-col gap-8 '>
-            <h2 className='text-6xl my-8 text-gold'>BASALT</h2>
-            <div className='flex gap-16 justify-evenly'>
-              <Image src={'/assets/images/basalt/basalt-lamp-1.jpeg'} width={300} height={300} alt='basalt-lamp' />
+        <div className='px-4 md:px-8'>
+          <div className='flex flex-col gap-8 mb-32'>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>BASALT</h2>
+            <div className='flex flex-col md:flex-row gap-4 md:gap-16 justify-evenly'>
+              <Image
+                src={'/assets/images/basalt/basalt-lamp-1.jpeg'}
+                width={300}
+                height={300}
+                alt='basalt-lamp'
+                className='mx-auto md:mx-0'
+              />
               <div className='flex flex-col'>
-                <h3 className='text-4xl my-8 text-gold'>Basalt Pendant Lamp</h3>
-                <p className='text-white'>
+                <h3 className='text-2xl md:text-4xl my-4 md:my-8 text-gold'>Basalt Pendant Lamp</h3>
+                <p className='text-white text-sm md:text-base'>
                   Transform your living space with the modern elegance of our Contemporary Basalt Pendant Lamp. Crafted
                   from natural basalt stone, this pendant lamp adds a unique and stylish touch to any room with its
                   textured surface and warm, inviting glow.
                 </p>
-                <div className='text-white mt-8'>SIZE: 18 x 8 x 8 inches</div>
-                <div className='text-white mt-8 text-xl text-amber-100'>$199.99</div>
+                <div className='text-white mt-4 md:mt-8 text-sm md:text-base'>SIZE: 18 x 8 x 8 inches</div>
+                <div className='text-white mt-4 md:mt-8 text-lg md:text-xl text-amber-100'>$199.99</div>
               </div>
             </div>
           </div>
 
-          <div className='flex flex-col gap-8'>
-            <h2 className='text-6xl my-8 text-gold'>MARBLE</h2>
-            <div className='flex gap-16 justify-evenly'>
+          <div className='flex flex-col gap-8 mb-32'>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>MARBLE</h2>
+            <div className='flex flex-col md:flex-row gap-4 md:gap-16 justify-evenly'>
               <div className='flex flex-col'>
-                <h3 className='text-4xl my-8 text-gold'>Marble Table Lamp</h3>
-                <p className='text-white'>
+                <h3 className='text-2xl md:text-4xl my-4 md:my-8 text-gold'>Marble Table Lamp</h3>
+                <p className='text-white text-sm md:text-base'>
                   Illuminate your space with sophistication and style using our Elegant Marble Table Lamp. Crafted with
                   a luxurious marble base that showcases stunning natural veining, each lamp is a unique work of art.
                   The smooth, rounded edges of the marble create a modern yet timeless aesthetic, making it a perfect
                   addition to any room.
                 </p>
-                <div className='text-white mt-8'>SIZE: 20 x 8 x 14 inches</div>
-                <div className='text-white mt-8 text-xl text-amber-100'>$129.99</div>
+                <div className='text-white mt-4 md:mt-8 text-sm md:text-base'>SIZE: 20 x 8 x 14 inches</div>
+                <div className='text-white mt-4 md:mt-8 text-lg md:text-xl text-amber-100'>$129.99</div>
               </div>
-              <Image src={'/assets/images/marble/marble.jpeg'} width={300} height={300} alt='basalt-lamp' />
+              <Image
+                src={'/assets/images/marble/marble.jpeg'}
+                width={300}
+                height={300}
+                alt='marble-lamp'
+                className='mx-auto md:mx-0'
+              />
             </div>
           </div>
 
           <div className='flex flex-col gap-8'>
-            <h2 className='text-6xl my-8 text-gold'>GRANITE</h2>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>GRANITE</h2>
 
-            <div className='flex gap-16 justify-evenly'>
-              <Image src={'/assets/images/granite/granite.jpeg'} width={300} height={300} alt='basalt-lamp' />
+            <div className='flex flex-col md:flex-row gap-4 md:gap-16 justify-evenly'>
+              <Image
+                src={'/assets/images/granite/granite.jpeg'}
+                width={300}
+                height={300}
+                alt='granite-lamp'
+                className='mx-auto md:mx-0'
+              />
 
               <div className='flex flex-col'>
-                <h3 className='text-4xl my-8 text-gold'>Outdoor Carved Granite Lamp</h3>
-                <p className='text-white'>
+                <h3 className='text-2xl md:text-4xl my-4 md:my-8 text-gold'>Outdoor Carved Granite Lamp</h3>
+                <p className='text-white text-sm md:text-base'>
                   Elevate your outdoor space with the striking elegance of our Outdoor Carved Granite Lamp. This
                   beautifully crafted lamp features intricate carvings that create stunning light patterns, adding a
                   touch of artistry and sophistication to your garden, patio, or pathway.
                 </p>
-                <div className='text-white mt-8'>SIZE: 18 x 8 x 8 inches </div>
-                <div className='text-white mt-8 text-xl text-amber-100'>$149.99</div>
+                <div className='text-white mt-4 md:mt-8 text-sm md:text-base'>SIZE: 18 x 8 x 8 inches </div>
+                <div className='text-white mt-4 md:mt-8 text-lg md:text-xl text-amber-100'>$149.99</div>
               </div>
             </div>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -19,24 +19,24 @@ const Home: React.FC = () => {
             className='w-full h-auto'
           />
         </div>
-        <div className='px-8'>
-          <div className='mb-32'>
-            <h2 className='text-6xl my-8 text-gold'>VISION</h2>
-            <p className='text-2xl whitespace-pre-wrap text-white'>
+        <div className='px-4 md:px-8'>
+          <div className='mb-16 md:mb-32'>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>VISION</h2>
+            <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>
               {`To be the leading creator of culturally inspired lighting solutions\n that preserve heritage and illuminate the beauty of tradition in modern living spaces worldwide.`}
             </p>
           </div>
-          <div className='mb-32'>
-            <h2 className='text-6xl my-8 text-gold content-center'>ABOUT US</h2>
-            <div className='flex mb-8 content-center'>
-              <p className='text-2xl text-white mr-8'>{`At SOLA, we believe in the power of light to illuminate not just spaces but also the rich heritage and traditions that shape our world. We are a passionate team dedicated to preserving cultural heritage through innovative and beautifully crafted lighting solutions.`}</p>
-              <Image src={'/assets/images/logo.png'} width={300} height={100} alt='SOLA' />
+          <div className='mb-16 md:mb-32'>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold content-center'>ABOUT US</h2>
+            <div className='flex flex-col md:flex-row mb-8 content-center'>
+              <p className='text-lg md:text-2xl text-white md:mr-8 mb-4 md:mb-0'>{`At SOLA, we believe in the power of light to illuminate not just spaces but also the rich heritage and traditions that shape our world. We are a passionate team dedicated to preserving cultural heritage through innovative and beautifully crafted lighting solutions.`}</p>
+              <Image src={'/assets/images/logo.png'} width={240} height={100} alt='SOLA' className='mx-auto md:mx-0' />
             </div>
           </div>
 
-          <div className='mb-32'>
-            <h2 className='text-6xl my-8 text-gold'>MISSION</h2>
-            <p className='text-2xl whitespace-pre-wrap text-white'>
+          <div className='mb-16 md:mb-32'>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>MISSION</h2>
+            <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>
               {`We aim to.. 
 
 1) Preserve Cultural Heritage 
@@ -47,23 +47,23 @@ const Home: React.FC = () => {
             </p>
           </div>
           <div>
-            <h2 className='text-6xl my-8 text-gold'>CONTACT US</h2>
+            <h2 className='text-4xl md:text-6xl my-4 md:my-8 text-gold'>CONTACT US</h2>
             <div className='flex flex-col gap-2'>
-              <p className='text-2xl whitespace-pre-wrap text-white'>{`For more information, please contact us at `}</p>
-              <div className='flex gap-2'>
+              <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>{`For more information, please contact us at `}</p>
+              <div className='flex items-center gap-2'>
                 <Image src={'/assets/icons/phone.svg'} alt='phone' width={32} height={32} />
-                <p className='text-2xl whitespace-pre-wrap text-white'>{`(031) 2981005`}</p>
+                <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>{`(031) 2981005`}</p>
               </div>
-              <div className='flex gap-2'>
+              <div className='flex items-center gap-2'>
                 <Image src={'/assets/icons/address.svg'} alt='address' width={32} height={32} />
-                <p className='text-2xl whitespace-pre-wrap text-white'>
+                <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>
                   {`Jl. Raya Kalirungkut, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293`}
                 </p>
               </div>
 
-              <div className='flex gap-2'>
+              <div className='flex items-center gap-2'>
                 <Image src={'/assets/icons/email.svg'} alt='email' width={32} height={32} />
-                <p className='text-2xl whitespace-pre-wrap text-white'>{`sola.luxury@gmail.com`}</p>
+                <p className='text-lg md:text-2xl whitespace-pre-wrap text-white'>{`sola.luxury@gmail.com`}</p>
               </div>
             </div>
           </div>
